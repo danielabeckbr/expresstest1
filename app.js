615728f2f39921bf1d7b591bb6fcf06fccacca4e
danielabeckbr <daniela.beck@br.de>
2022-10-17 16:29:14 +0200

    jetztmitrouter

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 
+
 app.get('/', (req,res) => {
   res.send("Hello");
 });
+
+app.get('/about', function (req, res) {
+  res.send("about what?");
+});
+
 
 app.listen(process.env.port || 3000);
 console.log('Web Server is listening at port '+ (process.env.port || 3000));
